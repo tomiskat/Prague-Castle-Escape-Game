b3724079ca00394fd6012e498e0c15d60d0d13a0
tomiskat <tadeastomiska@email.cz>
2025-05-23 10:23:13 +0200

    Closed testing version added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "prague.castle.escape"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
