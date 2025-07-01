cf2c8135d2dd02a7feb4389d19f897583732d658
tomiskat <tadeastomiska@email.cz>
2025-07-01 23:37:36 +0200

    New version released

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "prague.castle.escape"
         minSdk = 29
         targetSdk = 35
-        versionCode = 3
+        versionCode = 5
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
